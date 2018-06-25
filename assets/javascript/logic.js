303396fe6aa0a9b53051f049aacb31beb90b4d3d
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-06-25 18:42:39 -0500

    fix

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+$(document).ready(function() { // DOCUMENT READY OPENING
+
+var config = {
+    apiKey: "AIzaSyBOQCI22eh_JRFqj1wjjTasZATxwKk591k",
+    authDomain: "train-schedule-18b48.firebaseapp.com",
+    databaseURL: "https://train-schedule-18b48.firebaseio.com",
+    projectId: "train-schedule-18b48",
+    storageBucket: "",
+    messagingSenderId: "867496002486"
+  };
+  firebase.initializeApp(config);
+
+}); // DOCUMENT READY CLOSING
